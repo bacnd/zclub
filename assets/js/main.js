2a36013d2360ae3eefd000d57cc4684b810fa949
Dinh Bac <nguyendinhbac99@gmail.com>
2020-11-04 14:47:06 +0700

    fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,7 @@ $(function () {
     $('.form-group > .pw').on('click', function () {
         console.log($(this).find('img').attr('src'));
         $(this).hasClass('eye-view') ? ($(this).removeClass('eye-view'), $(this).parent().children('input').attr('type', 'password')) : ($(this).addClass('eye-view'), $(this).parent().children('input').attr('type', 'text'));
-        var src = ($(this).find('img').attr('src') === 'assets/images/eyes.png' ? 'assets/images/eyes-gach.png' : 'assets/images/eyes.png');
+        var src = $(this).find('img').attr('src') === 'assets/images/eyes.png' ? 'assets/images/eyes-gach.png' : 'assets/images/eyes.png';
         $(this).find('img').attr('src', src);
     });
 
@@ -23,6 +23,10 @@ $(function () {
                 minlength: 6,
                 equalTo: '#pwd',
             },
+            // code: {
+            //     required: true,
+            //     maxlength: 4,
+            // },
         },
         messages: {
             username: {
@@ -38,10 +42,14 @@ $(function () {
                 minlength: 'Mật khẩu phải dài ít nhất 6 ký tự',
                 equalTo: 'Nhập lại mật khẩu không đúng',
             },
+            // code: {
+            //     required: 'Vui lòng nhập capcha',
+            //     maxlength: 'Mã capcha dài không quá 4 ký tự',
+            // },
         },
         // errorElement: 'span',
         submitHandler: function (form) {
-            form.submit();
+            // form.submit();
         },
     });
 
@@ -72,47 +80,82 @@ $(function () {
         // $('html, body').animate({ scrollTop: $(document).height() }, 1000);
     });
 
-    $(window).on('load resize', function(){
-        if(getMobileOperatingSystem() === 'Android') {
+    $(window).on('load resize', function () {
+        if (getMobileOperatingSystem() === 'Android') {
             $('.android').show();
             $('.ios').hide();
         } else {
             $('.android').hide();
             $('.ios').show();
         }
-        
-        var win = $(this);
-        if (win.width() > 768) {
-            $('body').addClass('pc').removeClass('sp');
-            var src = $('.logo img').attr('src');
-            $('#form').show();
-            if (src === 'assets/images/logo-sp.png') {
-                $('.logo img').attr('src', 'assets/images/logo.png');
-            }
-        } else {
-            $('body').addClass('sp').removeClass('pc');
-            $('#form').hide();
-            var src = $('.logo img').attr('src');
-            if (src === 'assets/images/logo.png') {
-                $('.logo img').attr('src', 'assets/images/logo-sp.png');
-            }
-        }
+
+        // var win = $(this);
+        // if (win.width() > 768) {
+        //     $('body').addClass('pc').removeClass('sp');
+        //     var src = $('.logo img').attr('src');
+        //     $('#form').show();
+        //     // if (src === 'assets/images/logo-sp.png') {
+        //     //     $('.logo img').attr('src', 'assets/images/logo.png');
+        //     // }
+        // } else {
+        //     $('body').addClass('sp').removeClass('pc');
+        //     $('#form').hide();
+        //     var src = $('.logo img').attr('src');
+        //     // if (src === 'assets/images/logo.png') {
+        //     //     $('.logo img').attr('src', 'assets/images/logo-sp.png');
+        //     // }
+        // }
     });
 
-    $('.tabs .tab-item').click(function() {
-        var tab = $(this).attr("data-tab");
+    loadCapcha(),
+        $('.btn-reload').click(function () {
+            loadCapcha();
+        });
+
+    // $('#registerForm').submit(function (e) {
+    //     e.preventDefault();
+    //     alert($(this).serialize());
+    //     // var values = $(this).serialize()
+    //     // logic....
+    //     return false;
+    // });
+
+    $('#registerForm').submit(function (e) {
+        e.preventDefault();
+        var user = $('#username').val();
+        var password = $.md5($('#pwd').val());
+        var idCode = $('.code-img img').data('id');
+        $.ajax({
+            type: 'GET',
+            url: 'http://portal.zclub.vin/api?c=1&un=' + user + '&pw=' + password + '&cp=xaf9&cid=' + idCode + '&at=',
+            dataType: 'json',
+            contentType: false,
+            processData: false,
+            cache: false,
+            success: function (data) {
+                console.log(data);
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                console.log(textStatus, errorThrown);
+            },
+        });
+        return false;
+    });
+
+    $('.tabs .tab-item').click(function () {
+        var tab = $(this).attr('data-tab');
         $('.tabs .tab-item').removeClass('active');
         $(this).addClass('active');
-        if(tab === 'register') {
+        if (tab === 'register') {
             var src = $('.tab-item.register img').attr('src');
             if (src === 'assets/images/reg.png') {
                 $('.tab-item.register img').attr('src', 'assets/images/reg-active.png');
             }
-            
+
             var srclogin = $('.tab-item.login img').attr('src');
-            if(srclogin === 'assets/images/log-active.png') {
+            if (srclogin === 'assets/images/log-active.png') {
                 $('.tab-item.login img').attr('src', 'assets/images/log.png');
-            }            
+            }
 
             $('#registerForm').show();
             $('#loginForm').hide();
@@ -121,11 +164,11 @@ $(function () {
             if (src === 'assets/images/log.png') {
                 $('.tab-item.login img').attr('src', 'assets/images/log-active.png');
             }
-            
+
             var srcregister = $('.tab-item.register img').attr('src');
-            if(srcregister === 'assets/images/reg-active.png') {
+            if (srcregister === 'assets/images/reg-active.png') {
                 $('.tab-item.register img').attr('src', 'assets/images/reg.png');
-            }  
+            }
 
             $('#registerForm').hide();
             $('#loginForm').show();
@@ -135,20 +178,39 @@ $(function () {
 
 function getMobileOperatingSystem() {
     var userAgent = navigator.userAgent || navigator.vendor || window.opera;
-  
-        // Windows Phone must come first because its UA also contains "Android"
-      if (/windows phone/i.test(userAgent)) {
-          return "Windows Phone";
-      }
-  
-      if (/android/i.test(userAgent)) {
-          return "Android";
-      }
-  
-      // iOS detection from: http://stackoverflow.com/a/9039885/177710
-      if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
-          return "iOS";
-      }
-  
-      return "unknown";
-  }
+
+    // Windows Phone must come first because its UA also contains "Android"
+    if (/windows phone/i.test(userAgent)) {
+        return 'Windows Phone';
+    }
+
+    if (/android/i.test(userAgent)) {
+        return 'Android';
+    }
+
+    // iOS detection from: http://stackoverflow.com/a/9039885/177710
+    if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+        return 'iOS';
+    }
+
+    return 'unknown';
+}
+
+function loadCapcha() {
+    $.ajax({
+        type: 'GET',
+        url: 'http://portal.zclub.vin/api?c=124&type=0&at=',
+        dataType: 'json',
+        contentType: false,
+        processData: false,
+        cache: false,
+        success: function (data) {
+            var base64_string = data.img;
+            var img = 'data:image/png;base64,' + base64_string;
+            $('.code-img img').attr('src', img).attr('data-id', data.id);
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            console.log(textStatus, errorThrown);
+        },
+    });
+}
